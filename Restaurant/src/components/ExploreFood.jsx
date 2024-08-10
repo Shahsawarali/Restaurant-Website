2aa66db82e04ee2925d1d478a9f9d9275cb48f1c
IlOvecompUter1$t <shahva2022@gmail.com>
2024-08-10 10:37:38 +0500

    hero section style done

---
+++ Restaurant/src/components/ExploreFood.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExploreFood() {
+  return (
+    <div className='explore-food'>
+        <h1>ExploreFood</h1>
+        </div>
+  )
+}
+
+export default ExploreFood
